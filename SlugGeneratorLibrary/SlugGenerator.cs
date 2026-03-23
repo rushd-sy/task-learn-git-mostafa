c578d8366d6e2d48293bb77b83f0067916b62194
Mostafa Shraief <mostafashraief5@gmail.com>
2026-03-23 22:09:29 +0300

    add: implement slug convertor and unit tests

---
+++ SlugGeneratorLibrary/SlugGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SlugGeneratorLibrary
+{
+    public static class SlugGenerator
+    {
+        public static string Generate(string text)
+        {
+            if (text is null)
+                throw new ArgumentNullException(nameof(text));
+            text = text.Trim()
+                .ToLowerInvariant()
+                .Replace("$", "dollar")
+                .Replace("&", "and");
+            text = Regex.Replace(text, @"[+()^*%#@!/\\.,|`~]+", string.Empty);
+            text = Regex.Replace(text, @"[\s_-]+", "-");
+            return text;
+        }
+    }
+}
