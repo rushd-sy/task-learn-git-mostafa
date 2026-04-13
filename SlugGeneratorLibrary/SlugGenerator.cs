9835c3bfca375f3c39599908a8d3db678b36b11b
Mostafa Shraief <mostafashraief5@gmail.com>
2026-04-13 20:06:14 +0300

    feat: ability to use a custom separator character

--- SlugGeneratorLibrary/SlugGenerator.cs
+++ SlugGeneratorLibrary/SlugGenerator.cs
@@ -5,23 +5,34 @@ namespace SlugGeneratorLibrary
 {
     public static class SlugGenerator
     {
-        public static string Generate(string text)
+        public static string CustomGenerate(string text, char separator)
         {
             if (text is null)
                 throw new ArgumentNullException(nameof(text));
             text = text.Trim()
                 .ToLowerInvariant();
             text = Regex.Replace(text, @"[+()^*%#@!/\\.,|`~]+", string.Empty);
-            text = Regex.Replace(text, @"[\s_-]+", "-");
+            text = Regex.Replace(text, @"[\s_-]+", separator.ToString());
             return text;
+        }
+
+        public static string GenerateHyphens(string text)
+        {
+            return CustomGenerate(text, '-');
+        }
+
+        public static string GenerateUnderscores(string text)
+        {
+            return CustomGenerate(text, '_');
         }
     }
 
     public static class StringExtensions
     {
+        /// <summary>Convert string to slug separated by hyphens.</summary>
         public static string ToSlug(this string text)
         {
-            return SlugGenerator.Generate(text);
+            return SlugGenerator.GenerateHyphens(text);
         }
     }
 }
